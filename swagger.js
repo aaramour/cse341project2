6ae2a8547be5bcdec7e3189a662c9995e69703f5
aaramour@byui.edu <aaramour@byui.edu>
2024-05-23 16:59:56 -0400

    updated render link in swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
       title: 'My API',
       description: 'Recipes API'
     },
-    host: 'cse341w04pa.onrender.com',
+    host: 'cse341project2ae.onrender.com',
     schemes: ['https']
 };
  
