c5f9c8067c31e34faf35e3ce952bb4222d9fa681
aaramour@byui.edu <aaramour@byui.edu>
2024-06-06 23:28:32 -0400

    start implement oauth

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const clientID = process.env.CLIENT_ID;
 
 router.use('/', require('./swagger'));
 router.use('/recipes', require('./recipes'));
+router.use('/auth', (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`)
+});
+// router.get('/', function(req, res) {
+//     res.render('./pages/index',{client_id: clientID});
+//   })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 const mongodb = require('./db/connection')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+const axios = require('axios');
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+app.set('view engine', 'ejs');
+var access_token = '';
 
 app
   .use(bodyParser.json())
@@ -23,6 +29,36 @@ app
     next();
   })
   .use('/', require('./routes'))
+  .get('/', function(req, res) {
+    res.render('pages/index',{client_id: clientID});
+  })
+  .get('/oauth-callback', (req, res) => {
+    const requestToken = req.query.code
+    axios({
+      method: 'post',
+      url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+      // Set the content type header, so that we get the response in JSON
+      headers: {
+           accept: 'application/json'
+      }
+    }).then((response) => {
+      access_token = response.data.access_token
+      res.redirect('/success');
+    })
+  })
+  
+  app.get('/success', function(req, res) {
+  
+    axios({
+      method: 'get',
+      url: `https://api.github.com/user`,
+      headers: {
+        Authorization: 'token ' + access_token
+      }
+    }).then((response) => {
+      res.render('pages/success',{ userData: response.data });
+    })
+  });
   
 
 mongodb.initDb((err, mongodb ) => {
